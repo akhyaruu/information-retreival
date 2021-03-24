8d17c2d9d7975d39a634769950410d60d3578ffe
ilham akhyar <ilhamakhyar202@gmail.com>
2021-03-24 21:07:39 +0700

    still fixing dictionary

--- main.js
+++ main.js
@@ -1,28 +1,30 @@
 /* 
-   Initial stage of text processing (pakai bhs. inggris)
+   Tahapan text processing dengan inverted index (https://nlp.stanford.edu/IR-book/html/htmledition/a-first-take-at-building-an-inverted-index-1.html)
 
-   1. Tokenization
-   2. Normalization
-   3. Stemming
-   4. Stop words
+   1. Collect documents
+   2. Tokenization
+   3. Normalization
+   4. Stemming (linguistic preprocessing)
+   5. Stop words
+   6. Index the document consisting dictionary and postings
 
    ###################
 
-   1. Tokenization
+   2. Tokenization
       # cut character sequence into words tokens #
       cara baca regex nya (semua karakter yang bukan karakter word atau whitespace)
 
-   2. Normalization
+   3. Normalization
       # map text and query term to same form #
       ex: U.S.A sama seperti USA
       ini gk perlu soalnya di tokenize sudah disederhanakan banget
     
-   3. Stemming
+   4. Stemming
       # we may wish different forms of a root to match #
       ex: authorize, authorization
       soon dikerjakan, sekarang gk
       
-   4. Stop words
+   5. Stop words
       # We may omit very common words (or not) #
       ex: the, a, to, of, etc 
 */
@@ -37,25 +39,35 @@ const data1 = file1.split('\r\n');
 const data2 = file2.split('\r\n');
 const data = data1.concat(data2);
 
+const countData1 = data1.length;
+const countData2 = data2.length;
+const total = countData1 + countData2;
+
 
 textProcessing(data);
 
 
 function textProcessing(data) {
-   // data.map(line => {
-   //    tokenize(line);
-   // });
 
-   const textResult = [];
+   const tokenWords = [];
    data.map(line => {
       const arrWords = tokenize(line);
-      arrWords.map(word => textResult.push(word));
+      arrWords.map(word => tokenWords.push(word));
    });
-   console.log(textResult);
+
+   // const kata = 'ilham akhyar firdaus';
+   // const dicari = 'firdaus'
+   // let re = new RegExp("\\b(" + dicari + ")\\b","g");
+   // console.log(kata.match(re));
+
+   const result = indexDocument(tokenWords);
+
+
 }
 
 
-function tokenize(line) {
+function tokenize(line) { 
+   /* memecah kalimat menjadi token kata */
    const lineLower = line.toLowerCase();
    const regex =  /[^\w\s]/g; 
    const charMatch = lineLower.match(regex);
@@ -69,8 +81,8 @@ function tokenize(line) {
    return prettier(stopwords(arrWords));
 }
 
-
 function stopwords(arrWords) {
+   /* menghilangkan stopwords di bhs. inggris */
    const sourceStopWords = fs.readFileSync('src/stopwords.txt', 'utf-8').toString();
    const stopWords = sourceStopWords.split('\r\n');
    stopWords.map(stopword => {
@@ -86,12 +98,74 @@ function stopwords(arrWords) {
    return arrWords;
 }
 
-
 function prettier(arrWords) {
-   // if (arrWords.length) {
-   //    const words = arrWords.filter(word => word.length != 0);
-   //    return words;
-   // } 
-      
+   /* menghilangkan whitespace dan value yg kosong */
    return arrWords.filter(word => word.length != 0).filter(arr => arr);   
+}
+
+function documentCleaning(doc) {
+   /* membersihkan dokumen dari karakter agar mudah ditemukan pattern nya */
+   const newDoc = [];
+   doc.map(line => {
+      const lineLower = line.toLowerCase();
+      let lineReplace = lineLower;
+      const charMatch = line.match(/[^\w\s]/g);
+      if (charMatch) {
+         charMatch.map(char => {
+            lineReplace = lineReplace.replace(char, '');
+         });   
+      }
+      newDoc.push(lineReplace);
+   });
+   return newDoc;
+}
+
+/*  dari https://medium.com/dailyjs/how-to-remove-array-duplicates-in-es6-5daa8789641c  */
+function removeDuplicate(arr) {
+   [... new Set(arr)];
+   arr.filter((item, index) => arr.indexOf(item) === index);
+   return arr.reduce((unique, item) => unique.includes(item) ? unique : [...unique, item], []);
+}
+
+function indexDocument(tokenWords) {
+   const newToken = removeDuplicate(tokenWords).sort();
+   const dictionaries = [];
+   const doc = documentCleaning(data);
+   newToken.map(word => {
+      const regex = new RegExp("\\b(" + word + ")\\b","g");
+      doc.map((line, index) => {
+         if (line.match(regex)) {
+            if ((index + 1) <= countData1) {
+               dictionaries.push({term: word, docId: 1});
+            } else {
+               dictionaries.push({term: word, docId: 2});
+            }
+         }
+      });
+   });
+
+   // console.log(dictionaries);
+   const result = [];
+   dictionaries.map((dic1, index1) => {
+      dictionaries.filter((dic2, index2) => {
+         if (index1 == index2) {
+            return false;
+         }
+
+         if (dic1.term === dic2.term) {
+            result.push({term: dic1, docId: [dic1.docId, dic2.docId]});
+            return false;
+         } else {
+            result.push({term: dic1, docId: dic1.docId});
+            return false;
+         }
+      });
+   });
+
+
+   // if (dic1.term === dic2.term) {
+   //    result.push({term: dic1, docId: [dic1.docId, dic2.docId]});
+   // }
+   // console.log(JSON.stringify(result, null, 3));
+   console.log(result);
 }
